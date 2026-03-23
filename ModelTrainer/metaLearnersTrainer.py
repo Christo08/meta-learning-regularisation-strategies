55cb3324c5a3c3f34cd958bde31cc2c5ec3f6c70
Christiaan Opperman <ChristoOpperman0@gmail.com>
2026-03-23 08:55:05 +0200

    Remove old folders

--- ModelTrainer/metaLearnersTrainer.py
+++ ModelTrainer/metaLearnersTrainer.py
@@ -1,4 +1,4 @@
-
+import os
 from datetime import datetime
 import random
 import pandas as pd
@@ -8,7 +8,7 @@ from ModelTrainer.knnTrainer import training_all_k_nearest_neighbors
 from ModelTrainer.randomForestTrainer import training_all_random_forests
 from ModelTrainer.svmTrainer import training_all_support_vector_machines
 from ModelTrainer.nnTrainer import training_all_neural_networks
-from Utils.constants import META_LEARN_TYPES
+from Utils.constants import META_LEARN_TYPES, RESULTS_PATH
 from Utils.fileHandler import save_data_frame
 from Utils.menus import show_meta_leaner_type_menu
 
@@ -17,8 +17,13 @@ def train_meta_learners(training_dataset, testing_dataset):
     selected_meta_learn_types = show_meta_leaner_type_menu()
     number_of_folds = int(input("How many folds do you want to use per instance? "))
     results = pd.DataFrame(columns=["model type", "technique",  "training loses", "testing loses"])
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+    settings = {}
     for selected_meta_learn_type in selected_meta_learn_types:
         settings_file_path = input(f"Enter the path of the {selected_meta_learn_type}' settings file: ")
+        settings[selected_meta_learn_type] = settings_file_path
+    for selected_meta_learn_type in selected_meta_learn_types:
+        settings_file_path = settings[selected_meta_learn_type]
         seed = random.randint(0, 4294967295)
         if selected_meta_learn_type == META_LEARN_TYPES[1]:
             result = training_all_decision_trees(settings_file_path , training_dataset, testing_dataset, seed, number_of_folds)
@@ -33,6 +38,7 @@ def train_meta_learners(training_dataset, testing_dataset):
         else:
             return
         results = pd.concat([results, pd.DataFrame(result)], ignore_index=True)
-    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-    fileName = f"Data/Datasets/Results/meta_learners_results_{timestamp}.csv"
-    save_data_frame(results, fileName)
+    if not os.path.isdir(RESULTS_PATH):
+        os.makedirs(RESULTS_PATH, exist_ok=True)
+    file_name = f"{RESULTS_PATH}meta_learners_results_{timestamp}.csv"
+    save_data_frame(results, file_name)
