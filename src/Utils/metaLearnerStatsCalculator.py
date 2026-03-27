f0830dc20776451a320fb797ccf3cf4e96fdd36c
Christiaan Opperman <ChristoOpperman0@gmail.com>
2026-03-27 13:04:20 +0200

    Update graphs

--- src/Utils/metaLearnerStatsCalculator.py
+++ src/Utils/metaLearnerStatsCalculator.py
@@ -13,6 +13,10 @@ class TrainingMetaLearnerStats:
         self.testing_mses = []
         self.testing_f1 = []
         self.testing_accuracy = []
+        self.testing_true_negative = 0
+        self.testing_false_negative = 0
+        self.testing_true_positive = 0
+        self.testing_false_positive = 0
 
     def update_stats(self, y_training, y_train_pred, y_testing, y_test_pred):
         self.training_mses.append(mean_squared_error(y_training, y_train_pred))
@@ -22,6 +26,7 @@ class TrainingMetaLearnerStats:
         self.testing_mses.append(mean_squared_error(y_testing, y_test_pred))
         self.testing_f1.append(f1_score(y_testing, y_test_pred, average='weighted'))
         self.testing_accuracy.append(accuracy_score(y_testing, y_test_pred)*100)
+        self.testing_true_negative, self.testing_false_negative, self.testing_true_positive, self.testing_false_positive = tp_tn_fp_fn(y_testing, y_test_pred)
 
     def get_stats_json_object(self):
         return {
@@ -30,7 +35,11 @@ class TrainingMetaLearnerStats:
         "training f1": self.training_f1,
         "testing loses": self.testing_mses,
         "testing f1": self.testing_f1,
-        "testing accuracies": self.testing_accuracy
+        "testing accuracies": self.testing_accuracy,
+        "testing true positives": self.testing_true_positive,
+        "testing true negatives": self.testing_true_negative,
+        "testing false positives": self.testing_false_positive,
+        "testing false negatives": self.testing_false_negative
     }
 
 class TestingMetaLearnerStats:
