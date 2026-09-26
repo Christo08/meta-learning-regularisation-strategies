Fix class inheritance from Model to Module and add function to calculate average and standard deviation of losses

src/Models/NN/network.py
@@ -4,7 +4,7 @@ from torch.nn.utils import prune
 from torch.nn.utils.parametrizations import weight_norm
 
 
-class Network(nn.Model):
+class Network(nn.Module):
     def __init__(self,
                  input_size,
                  hidden_sizes,